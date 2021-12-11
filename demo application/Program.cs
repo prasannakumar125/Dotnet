cd0b1abd3d1fc3950d032311e847cf6f83313ec5
prasanna kumar <prasannadotnet6@gmail.com>
2021-12-11 08:32:43 +0530

    loops

---
+++ demo application/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo_application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Addition of two numbeers a=10,b=20,sum=a+b;
+            //Needs to declare the variables
+
+
+            int[] array = new int[] { 1, 3, 4, 5, 6, 6, 7, 7 };
+            foreach (int i in array)
+            {
+                Console.WriteLine("" + i);
+            }
+            Console.ReadLine();
+            //For loop and Foreach loop
+
+            //Foreach loop foreach(datatype variablename in collection)
+            //{
+
+            //}
+            //Syntax: for(initialization;Condition checking;increment/decrement operator)
+            //  {
+
+            //}
+            //int i = 0;
+            //Console.WriteLine("no");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //for(i=0;n>i;i++)
+            //{
+            //    Console.WriteLine("" + i);
+            //}
+            //Console.ReadLine();
+
+            ////loops
+            // do-while
+            //Console.WriteLine("PLease enetr no");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int i = 0;
+
+            //do
+            //{
+            //    Console.WriteLine("no of items" + i);
+            //    i++;
+            //}
+            //while (n > i);
+            //while(n>i)
+            //{
+            //     Console.WriteLine("" + i);
+            //i++;
+            //}
+
+            Console.ReadLine();
+
+            //Console.WriteLine("please enter the first number");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("please enter the second number");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //int sum = a + b;
+            //Console.WriteLine("sum of the two numbers" +sum);
+            //Console.ReadLine();
+
+        }
+    }
+}
+    
+
