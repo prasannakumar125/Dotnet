1baf29f58998153e2354102b8fe21df5f67f1f1d
prasanna kumar <prasannadotnet6@gmail.com>
2021-12-11 21:21:05 +0530

    additon

---
+++ Github practice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Github_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter the first number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("pleaseenter econd number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sum = a + b;
+            Console.WriteLine("sum of two numbers" + sum);
+            Console.ReadLine();
+        }
+    }
+}
